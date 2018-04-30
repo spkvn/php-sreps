02416987c5b567389656b955f6a28a59226e4bcd
spkvn <kevinmeagher1@hotmail.com>
2018-04-30 11:48:10 +1000

    basic bar graph with static data in reports index

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -12,64 +12,29 @@
     </div>
 
     <div class="row bg-white rounded-corners my-3 py-3 px-3">
-        <div class="col">
-
-            <!--<table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Code</th>
-                        <th scope="col">Total</th>
-                        <th scope="col">Date</th>
-                        {{--<th scope="col">Edit</th>--}}
-                        <th scope="col">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {{--
-                        Tip: For else directive
-                        Provides you with a foreach loop, and a @empty block if the
-                        array or collection passed in is empty!
-                    --}}
-                    @forelse($sales as $sale)
-                        <tr>
-                            <td>{{ $sale->id }}</td>
-                            <td>{{ $sale->customer }}</td>
-                            <td>{{ $sale->code }}</td>
-                            <td>{{ $sale->total }}</td>
-                            <td>{{ $sale->created_at->diffForHumans()}}</td>
-                            {{--<td><a class="btn btn-secondary" href="{{route('sales.edit', $sale->id)}}">Edit</a></td>--}}
-                            <td>
-                                <form class="delete-form" action="{{route('sales.destroy',$sale->id)}}" method="POST">
-                                    {{csrf_field()}}
-                                </form>
-                                <button class="delete-button btn btn-dark">Delete</button>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>No Sales</td>
-                            <td></td>
-                            {{--<td></td>--}}
-                            <td></td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>-->
-
+        <div class="col-12" class="controls">
+            <p class="lead">Controls</p>
         </div>
+        <div class="col-12">
+            <div class="bar-wrapper" id="reportsWrapper">
+
+            </div>
+        </div>
+
     </div>
 @endsection
-@section('javascript')
+@push('javascript')
 <script>
-    $('.delete-button').on('click',function(e){
-        e.preventDefault();
-        var $this = $(this);
-        var deleteForm = $this.siblings('.delete-form').submit();
-    });
+    var data = [234,231,302,284,299,253,321];
+    d3.select('#reportsWrapper')
+        .selectAll('div')
+        .data(data)
+        .enter().append('div')
+        .style("width", function(d){
+            return d * 2 +"px"
+        })
+        .text(function(d){
+            return d;
+        });
 </script>
-@endsection
+@endpush
